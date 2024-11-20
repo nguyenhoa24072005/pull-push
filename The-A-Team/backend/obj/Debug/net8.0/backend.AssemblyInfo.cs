73dd933b3d4b3b4363e4c3e9174a1cc700d68c2b
hoanguyen24072005 <nduc37039@gmail.com>
2024-11-20 17:33:07 +0700

    Thông điệp commit của bạn

--- The-A-Team/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ The-A-Team/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf8da71609dd84ab6dfff3465648d83e433108e")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
