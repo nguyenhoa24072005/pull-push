5c62693bb5011e95238647a26a63f45af67cec66
hoanguyen24072005 <nduc37039@gmail.com>
2024-11-27 22:54:47 +0700

    .

--- obj/Debug/net8.0/ComicRentalSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComicRentalSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComicRentalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73dd933b3d4b3b4363e4c3e9174a1cc700d68c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b1304709279f3f045cb0082db181e6220401a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComicRentalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComicRentalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
